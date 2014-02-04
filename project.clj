0eceaaa775750013bc0462efcb079dd716f921af
Lucas Stadler <mi.au@papill0n.org>
2014-02-04 12:54:15 +0100

    use correct `:source-paths`.

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject rikai "0.0-pre-anything"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.datomic/datomic-free "0.9.4497"]])
+                 [com.datomic/datomic-free "0.9.4497"]]
+  :source-paths ["."])
