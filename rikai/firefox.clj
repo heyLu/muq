5146785c542d8ef34b67c9e59bf67a469b7cd11d
Lucas Stadler <mi.au@papill0n.org>
2014-02-04 11:35:04 +0100

    parse the visits table as well.

--- rikai/firefox.clj
+++ rikai/firefox.clj
@@ -8,20 +8,53 @@
     (catch NumberFormatException ne
       nil)))
 
-(defn ->hist-entry [[url title last-visited]]
+(defn parse-long [n]
+  (try
+    (Long/parseLong n)
+    (catch NumberFormatException ne
+      nil)))
+
+(defn firefox-date [s]
+  (if-let [n (parse-long s)]
+    (Date. (long (/ n 1000)))))
+
+(defn ->hist-entry [[id url title last-visited]]
   (let [title (if (and (seq? title) (> (count title) 0))
                 (.substring 1 title (dec (count title))))
-        last-visited (parse-int last-visited)
-        t (type last-visited)]
+        last-visited (firefox-date last-visited)]
     (conj {:url url}
           (if title {:title title})
-          (if last-visited {:last-visited (Date. (unchecked-divide-int last-visited 1000))}))))
+          (if last-visited {:last-visited last-visited}))))
+
+(defn read-csv [file]
+  (->> (slurp file) string/split-lines (map #(string/split % #","))))
 
 (def firefox-history
   (->>
-    (slurp "ff-history.csv")
-    string/split-lines
-    (map #(string/split % #","))
+    (read-csv "ff-history.csv")
     (map ->hist-entry)))
 
 ;(identity (filter #(.contains (:url %) "oops") firefox-history))
+
+(defn visit-type [type]
+  "http://mxr.mozilla.org/mozilla-central/source/toolkit/components/places/nsINavHistoryService.idl#1173"
+  (case type
+    (1 4 5 6 8) :link
+    2 :typed
+    3 :bookmark
+    7 :download))
+
+(defn ->visit [[id from_visit hist_id date type]]
+  (let [from (parse-int from_visit)]
+    (conj {:id (parse-int id)
+           :hist_id (parse-int hist_id)
+           :date (firefox-date date)
+           :type (visit-type (parse-int type))}
+          (if (not= 0 from) {:from from}))))
+
+(def firefox-visits
+  (->>
+    (read-csv "ff-visits.csv")
+    (map ->visit)))
+
+(count firefox-visits)
