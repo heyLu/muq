ef371ecd80fb351bd0a994ddc1b640be40526fd5
Lucas Stadler <mi.au@papill0n.org>
2014-02-26 13:45:55 +0100

    make mutomic 'loadable'.
    e.g. comment out old/irrelevant/non-working code, put test cases in
    deftests and put things in the right order.

--- src/mutomic.clj
+++ src/mutomic.clj
@@ -2,12 +2,19 @@
   "A µcroscopic in-memory temporal database in memory.
 
 Trying to understand datomic, mostly."
-  (:require [clojure.set :as set]))
+  (:require [clojure.set :as set])
+  (:use [clojure.test :only (deftest is)]))
 
 (defn data-matches [a b]
   (if (symbol? a)
     {a b}
     (if (= a b) {} nil)))
+
+(deftest test-data-matches
+  (is (= (data-matches 'a 'b) '{a b}))
+  (is (= (data-matches 'a 3) '{a 3}))
+  (is (= (data-matches 3 3) {}))
+  (is (= (data-matches 3 4) nil)))
 
 (defn clause-matches [clause datom]
   (let [[ce ca cv] clause
@@ -19,7 +26,9 @@ Trying to understand datomic, mostly."
       (into {} (filter map? [me ma mv]))
       nil)))
 
-(clause-matches '[?e :name "Joe"] [:joe :name "Joe"])
+(deftest test-clause-matches
+  (is (= (clause-matches '[?e :name "Joe"] [:joe :name "Joe"])
+         '{?e :joe})))
 
 (defn bindings-consistent? [b1 b2]
   (let [common-keys (set/intersection (into #{} (keys b1)) (into #{} (keys b2)))]
@@ -50,39 +59,45 @@ Trying to understand datomic, mostly."
    [:joe :likes :flowers]
    [:joe :name "Joe"]])
 
-(filter #(clause-matches '[?e :name ?n] %) fred-julia-joe)
+(deftest test-filter-clauses
+  (is (= (count (filter #(clause-matches '[?e :name ?n] %) fred-julia-joe))
+         3)))
 
-(defn merge-if-consistent [b1 b2]
-  (if (bindings-consistent? b1 b2)))
+(comment
+  (defn merge-if-consistent [b1 b2]
+    (if (bindings-consistent? b1 b2)))
 
-(defn clauses-match [clauses datoms]
-  (reduce (fn [bindings clause]
-            (let [new-bindings (clause-matches clause datom)]
-              (if bindings
-                (if (and new-binding (bindings-consistent? bindings new-binding))
-                  (conj bindings new-binding)
-                  nil)
-                nil)))
-          {}
-          clauses))
+  (defn clauses-match [clauses datoms]
+    (reduce (fn [bindings clause]
+              (let [new-bindings (clause-matches clause datom)]
+                (if bindings
+                  (if (and new-binding (bindings-consistent? bindings new-binding))
+                    (conj bindings new-binding)
+                    nil)
+                  nil)))
+            {}
+            clauses))
 
-(clauses-match '[[?e :name "Joe"]
-                 [?e :age 10]
-                 [?e :likes :julia]]
-               [:joe :name "Joe"])
-;=> {?e [[:name "Joe"] [:age 10] [:likes :julia]]}
+  (clauses-match '[[?e :name "Joe"]
+                   [?e :age 10]
+                   [?e :likes :julia]]
+                 [:joe :name "Joe"])
+  ;=> {?e [[:name "Joe"] [:age 10] [:likes :julia]]}
 
-'[[?j :name "Joe"]
-  [?j :friend ?p]
-  [?p :age ?a]
-  [(> ?a 10)]]
-;=> {?e [[:name "Joe"] [:friend ?p]]
-;    ?p [[:age ?a]]
-;    ?a [[(> 10)]]}
-; ?j -> ?p -> ?a
-; ?j => candidates for ?p
-; ?p => candidates for ?a
-; ?a => determines valid ?p and ?j
+
+  '[[?j :name "Joe"]
+    [?j :friend ?p]
+    [?p :age ?a]
+    [(> ?a 10)]]
+
+  ;=> {?e [[:name "Joe"] [:friend ?p]]
+  ;    ?p [[:age ?a]]
+  ;    ?a [[(> 10)]]}
+  ; ?j -> ?p -> ?a
+  ; ?j => candidates for ?p
+  ; ?p => candidates for ?a
+  ; ?a => determines valid ?p and ?j
+  )
 
 (defn variable? [v]
   (and (symbol? v) (.startsWith (name v) "?")))
@@ -104,14 +119,21 @@ Trying to understand datomic, mostly."
           {}
           clauses))
 
-(join-var-dependencies (join-vars story-clauses) story-clauses)
+;(join-var-dependencies (join-vars story-clauses) story-clauses)
 
 (defn root-vars [clauses]
   (filter (fn [var]
             (every? #(not (some #{var} (rest %))) clauses))
           (join-vars clauses)))
 
-(root-vars story-clauses)
+;(root-vars story-clauses)
+
+(defn root-var [clauses]
+  (or (first (root-vars clauses))
+      (let [join-var-deps (join-var-dependencies (join-vars clauses) clauses)]
+        (ffirst (sort-by (fn [[var deps]] (- (count deps))) join-var-deps)))))
+
+(root-var '[[?e :likes ?o] [?o :likes ?e]])
 
 (defn clause-dependencies [join-vars clause]
   (let [[e? a? v?] (map #(if (symbol? %) %) clause)
@@ -146,7 +168,7 @@ Trying to understand datomic, mostly."
                    nil))
                datoms)))
 
-(step {} '[?e :name ?n] fred-julia-joe)
+(step {} '[?e :likes ?o] fred-julia-joe)
 
 ; take one clause
 ; match it against the datoms -> set of matches that bind the var
@@ -162,42 +184,58 @@ Trying to understand datomic, mostly."
       nil)
     env))
 
-(resolve-var* {} '[[?e :name ?n] [?e :age ?a]] fred-julia-joe)
+(deftest test-resolve-var*
+  (let [friends-clauses '[[?e :likes ?o] [?o :likes ?e]]
+        friends (resolve-var* {} friends-clauses fred-julia-joe)]
+    (is (= (count friends) 2))))
 
 (defn resolve-var [var-name clauses datoms]
-  (let [clauses (join-clauses-with var-name clauses)]
+  (let [clauses (clauses-with var-name clauses)]
     (resolve-var* {} clauses datoms)))
 
-(resolve-var '?e '[[?e :name ?n] [?e :age ?a]] fred-julia-joe)
+(comment
+  (resolve-var '?e '[[?e :name ?n] [?e :age ?a]] fred-julia-joe)
+
+  (resolve-var '?o '[[?e :likes ?o] [?o :likes ?e]] fred-julia-joe)
+
+  ; does the wrong thing, e.g doesn't collect :name and :age for ?e, too.
+  ; maybe this would make sense if we resolve ?e in a separate step?
+  (resolve-var '?o '[[?e :name ?n]
+                     [?e :age ?a]
+                     [?e :likes ?o]
+                     [?o :likes ?e]] fred-julia-joe))
 
 (defn query-naive [clauses datoms]
   (resolve-var* {} clauses datoms))
 
-(resolve-var '?e '[[?e :name ?n]
-                   [?e :age ?a]
-                   [?e :likes ?o]
-                   [?o :likes ?e]] fred-julia-joe)
+(deftest test-query-naive
+  (let [friends-with-attrs '[[?e :name ?n]
+                             [?e :age ?a]
+                             [?e :likes ?o]
+                             [?o :likes ?e]]
+        friends (query-naive friends-with-attrs fred-julia-joe)]
+    (is (= (count friends) 2))))
 
-(query-naive '[[?e :name ?n] [?e :age ?a] [?e :likes ?o] [?o :likes ?e]] fred-julia-joe)
-; ?e -> ?tx -> ?user
+(comment
+  (def story-clauses
+    '[[?e :story/title ?v ?tx ?added]
+      [?e :story/url ?url]
+      [?tx :source/user ?user]
+      [?tx :db/txInstant ?inst]
+      [?user :user/email ?email]])
 
-(def story-clauses
-  '[[?e :story/title ?v ?tx ?added]
-    [?e :story/url ?url]
-    [?tx :source/user ?user]
-    [?tx :db/txInstant ?inst]
-    [?user :user/email ?email]])
-; ?e could depend on ?tx, but doesn't. order doesn't matter, so that can't determine it
-; either.
-; (result variables don't matter. removing some doesn't change the "shape" of the result.)
-; maybe knowledge of eav, could help? because we know that a `v` can be a reference
-; which might indicate that `e`s are more likely be non-dependent?
-; so maybe it's about vars that occur in the `e` position, but not in the `v` position?
+  ; ?e could depend on ?tx, but doesn't. order doesn't matter, so that can't determine it
+  ; either.
+  ; (result variables don't matter. removing some doesn't change the "shape" of the result.)
+  ; maybe knowledge of eav, could help? because we know that a `v` can be a reference
+  ; which might indicate that `e`s are more likely be non-dependent?
+  ; so maybe it's about vars that occur in the `e` position, but not in the `v` position?
 
-(dependencies story-clauses)
+  (dependencies story-clauses)
 
-;=> {?e [?v ?tx ?added ?url]
-;    ?tx [?user ?inst]
-;    ?user [?email]}
-;
-; variables should only appear as keys if they occur more than once?
+  ;=> {?e [?v ?tx ?added ?url]
+  ;    ?tx [?user ?inst]
+  ;    ?user [?email]}
+  ;
+  ; variables should only appear as keys if they occur more than once?
+  )
