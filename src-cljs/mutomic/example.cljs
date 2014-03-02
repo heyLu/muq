4382a2f1acfc602a36dabeae5d1a6f705c6dab2d
Lucas Stadler <mi.au@papill0n.org>
2014-03-02 12:07:55 +0100

    allow calling non-internal functions in cljs.
    they must be exported in some way, otherwise they won't be found. the
    code for munge is from the clojure and clojurescript compilers.

--- src-cljs/mutomic/example.cljs
+++ src-cljs/mutomic/example.cljs
@@ -7,3 +7,13 @@
  (mu/q '{:find [?name]
          :where [[_ :name ?name]]}
        mu/fred-julia-joe))
+
+(defn ^:export fancy? [name]
+  (> (count name) 4))
+
+(prn
+ (mu/q '{:find [?name ?age]
+         :where [[?p :name ?name]
+                 [?p :age ?age]
+                 [(mutomic.example/fancy? ?name)]]}
+       mu/fred-julia-joe))
--- src-cljs/mutomic/platform.cljs
+++ src-cljs/mutomic/platform.cljs
@@ -1,6 +1,7 @@
 (ns mutomic.platform
   "Host platform dependent code."
-  (:require [cljs.reader :as edn]))
+  (:require [cljs.reader :as edn]
+            [clojure.string :as string]))
 
 (defn starts-with [string start]
   (.startsWith string start))
@@ -11,8 +12,67 @@
 (defn illegal-argument [msg]
   (throw (js/Error. (str "Illegal argument: " msg))))
 
+(def munge-char-map
+  {\- "_"
+   \: "_COLON_"
+   \+ "_PLUS_"
+   \> "_GT_"
+   \< "_LT_"
+   \= "_EQ_"
+   \~ "_TILDE_"
+   \! "_BANG_"
+   \@ "_CIRCA_"
+   \# "_SHARP_"
+   \' "_SINGLEQUOTE_"
+   \" "_DOUBLEQUOTE_"
+   \% "_PERCENT_"
+   \^ "_CARET_"
+   \& "_AMPERSAND_"
+   \* "_STAR_"
+   \| "_BAR_"
+   \{ "_LBRACE_"
+   \} "_RBRACE_"
+   \[ "_LBRACK_"
+   \] "_RBRACK_"
+   \/ "_SLASH_"
+   \\ "_BSLASH_"
+   \? "_QMARK_"})
+
+(defn clj-munge [string]
+  "See clojure.lang.Compiler/munge"
+  (apply str (map #(munge-char-map % %) string)))
+
+(def js-reserved
+  #{"abstract" "boolean" "break" "byte" "case"
+    "catch" "char" "class" "const" "continue"
+    "debugger" "default" "delete" "do" "double"
+    "else" "enum" "export" "extends" "final"
+    "finally" "float" "for" "function" "goto" "if"
+    "implements" "import" "in" "instanceof" "int"
+    "interface" "let" "long" "native" "new"
+    "package" "private" "protected" "public"
+    "return" "short" "static" "super" "switch"
+    "synchronized" "this" "throw" "throws"
+    "transient" "try" "typeof" "var" "void"
+    "volatile" "while" "with" "yield" "methods"
+    "null"})
+
+(defn munge
+  "See cljs.compiler/munge"
+  ([s] (munge s js-reserved))
+  ([s reserved]
+   (let [ss (string/replace (str s) #"\\/(.)" ".$1")
+         ss (apply str (map #(if (reserved %) (str % "$") %)
+                            (string/split ss #"(?=\.)")))
+         ms (clj-munge ss)]
+     (if (symbol? s)
+       (symbol ms)
+       ms))))
+
 (defn resolve [sym]
-  nil)
+  (let [ns (namespace sym)
+        name (name sym)]
+    (apply aget js/window (conj (string/split (or ns "cljs.core") #"\.") (munge name)))))
 
 (defn read-edn [readers f]
   (throw (js/Error. "Not implemented!")))
