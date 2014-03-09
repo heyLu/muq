2b073afaa09c5f3c7d31b8f13dbc802eb42b5c5e
Lucas Stadler <lu@papill0n.org>
2014-03-09 18:27:22 +0100

    rename project and remove rikai dependencies.

--- project.clj
+++ project.clj
@@ -1,16 +1,9 @@
-(defproject rikai "0.1-dev"
+(defproject mutomic "0.1-dev"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2173"]
                  [org.clojure/data.fressian "0.2.0"]
-                 [com.datomic/datomic-free "0.9.4556"]
-
-                 ; service
-                 [ring "1.2.1"]
-                 [compojure "1.1.6"]
-                 [hiccup "1.0.5"]]
-  :plugins [[lein-ring "0.8.10"]
-            [lein-cljsbuild "1.0.2"]]
-  :ring {:handler rikai.service/app}
+                 [com.datomic/datomic-free "0.9.4556"]]
+  :plugins [[lein-cljsbuild "1.0.2"]]
   :cljsbuild {
     :builds [{:id "default"
               :source-paths ["src"]
