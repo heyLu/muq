0c94be58c6ab982b9579d937b957a5d314c23210
Lucas Stadler <lu@papill0n.org>
2014-03-12 15:38:23 +0100

    setup query example from cljs.
    and even support supplying custom data.

--- src/mutomic/example.cljs
+++ src/mutomic/example.cljs
@@ -1,5 +1,8 @@
 (ns mutomic.example
-  (:require [mutomic :as mu]))
+  (:require [mutomic :as mu]
+            [mutomic.natural :as n]
+            [clojure.string :as str]
+            [cljs.reader :as edn]))
 
 (enable-console-print!)
 
@@ -17,3 +20,19 @@
                  [?p :age ?age]
                  [(mutomic.example/fancy? ?name)]]}
        mu/fred-julia-joe))
+
+(defn ^:export setup-qa [question-id answer-id data-id]
+  (let [question-el (js/document.getElementById question-id)
+        answer-el (js/document.getElementById answer-id)
+        data-el (js/document.getElementById data-id)]
+    (when (str/blank? (.-value data-el))
+      (set! (.-value data-el) (pr-str mu/fred-julia-joe)))
+    (.addEventListener question-el "change"
+                       (fn [ev]
+                         (let [data (.-value data-el)
+                               data (if (str/blank? data)
+                                      mu/fred-julia-joe
+                                      (edn/read-string data))
+                               question (.-value question-el)
+                               res (n/mq question data)]
+                           (set! (.-textContent answer-el) (pr-str res)))))))
