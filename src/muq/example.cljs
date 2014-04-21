23140d386baf770824ac9bbf110a2a0e66c32944
Lucas Stadler <lu@papill0n.org>
2014-04-21 18:59:51 +0200

    first cut of a query demo.
    some parts are still weird:
    
    * after initial load you have to ctrl-enter in both fields (?)
    * error checking should be immediate
    * ...

--- src/muq/example.cljs
+++ src/muq/example.cljs
@@ -44,3 +44,56 @@
                        (fn [ev]
                          (if (= (.-keyCode ev) 13)
                            (answer-fn ev))))))
+
+(defprotocol KeyValueStore
+  (kv-read [s k])
+  (kv-write [s k v]))
+
+(def localStorage
+  (reify
+    KeyValueStore
+    (kv-read [_ k]
+             (when (.key js/localStorage (str k))
+               (edn/read-string (aget js/localStorage (str k)))))
+    (kv-write [_ k v]
+              (aset js/localStorage (str k) (pr-str v)))))
+
+(def query-data
+  (atom {:query '{:find [?person]
+                  :where [[?person :likes :julia]]}
+         :data s/fred-julia-joe}))
+
+(defn load-data! []
+  (reset! query-data (kv-read localStorage "muq.example.query")))
+
+(defn run-query! [query answer-el]
+  (try
+    (let [{:keys [query data]} query
+          result (mu/q query data)]
+      (set! (.-textContent answer-el) (pr-str result)))
+    (catch js/Error e
+      (set! (.-textContent answer-el) e))))
+
+(defn ^:export setup-query [query-id answer-id data-id]
+  (let [query-el (js/document.getElementById query-id)
+        answer-el (js/document.getElementById answer-id)
+        data-el (js/document.getElementById data-id)]
+    (add-watch query-data :rerun-query
+               (fn [_ _ _ new-query]
+                 (run-query! new-query answer-el)))
+    (add-watch query-data :store-data
+               (fn [_ _ old new]
+                 (when (not= old new)
+                   (kv-write localStorage "muq.example.query" new))))
+    (let [init-field! (fn [k el]
+                        (set! (.-textContent el) (pr-str (k @query-data)))
+                        (.addEventListener el "keyup"
+                                           (fn [ev]
+                                             (if (and (.-ctrlKey ev) (= (.-keyCode ev) 13))
+                                               (try
+                                                 (swap! query-data assoc k (edn/read-string (.-value el)))
+                                                 (catch js/Error e
+                                                   (set! (.-textContent answer-el) (str "Invalid " k ": " e))))))))]
+      (init-field! :query query-el)
+      (init-field! :data data-el))
+    (load-data!)))
